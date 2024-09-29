66908e89c9771b994fa0426c939cf5736cd7f98d
Zobayer <zobayermahmud00@gmail.com>
2024-09-30 01:18:20 +0600

    onboarding view

---
+++ app/src/main/java/com/example/nectar/ui/Splash.kt
@@ -0,0 +1,21 @@
+package com.example.nectar.ui
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import com.example.nectar.R
+
+class Splash : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@Splash, Onboarding::class.java))
+            finish()
+        }, 3000)
+    }
+}
